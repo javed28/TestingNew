07967564f701414b9d0a63ee17a3ee96c27971f3
gadgetzone <gadgetzone@gadgetzones-MacBook-Pro-3.local>
2018-11-05 18:10:12 +0530

    changed file

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be 12345 recreated.
     }
 
 }
