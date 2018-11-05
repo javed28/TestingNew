d7a9366c42afd3b66a9679e70482f28a035b867f
Irfan Shaikh <irfus555@gmail.com>
2018-11-05 18:06:17 +0530

    test commit irfan

--- testing/testingViewController.swift
+++ testing/testingViewController.swift
@@ -18,7 +18,7 @@ class testingViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be test recreated.
     }
     
 
